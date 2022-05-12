6709031ace81df7b90e9bb2d20c849248c84a8a5
jiyuujin <jiyuujinunite@gmail.com>
2022-05-12 22:31:42 +0900

    microCMS init

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,3 +1,5 @@
 export const variables = {
   liffId: import.meta.env.VITE_LIFF_ID,
+  microCmsApiKey: import.meta.env.VITE_MICROCMS_API_KEY,
+  microCmsApiEndpoint: import.meta.env.VITE_MICROCMS_API_ENDPOINT,
 }
