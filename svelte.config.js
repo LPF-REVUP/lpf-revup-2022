4d32fb37cdae32a78df53fb32d5f5c77643f983d
jiyuujin <jiyuujinunite@gmail.com>
2022-05-12 18:31:33 +0900

    fix: assets path

--- svelte.config.js
+++ svelte.config.js
@@ -17,6 +17,7 @@ const config = {
 
     paths: {
       base: process.env.NODE_ENV === 'development' ? '' : '/2022',
+      assets: process.env.NODE_ENV === 'development' ? '' : '/2022',
     },
 
     vite: {
