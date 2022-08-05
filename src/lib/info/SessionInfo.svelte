e80fd2dfce301e8907ba0902f8d6d2b79ff28bf7
Yuma Kitamura <jiyuujinunite@gmail.com>
2022-08-05 18:51:46 +0900

    Revert "brushup, others"

--- src/lib/info/SessionInfo.svelte
+++ src/lib/info/SessionInfo.svelte
@@ -2,10 +2,10 @@
   export let session
 </script>
 
-<div class="py-16 bg-revup-medium-brand">
-  <div class="container m-auto px-6 md:px-12 xl:px-6">
+<div class="py-16">
+  <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
     <div class="grid gap-8 md:grid-rows-2 lg:grid-cols-1">
-      <div class="row-span-2 p-6 rounded-xl text-center sm:p-8">
+      <div class="row-span-2 p-6 border border-gray-100 rounded-xl bg-gray-50 text-center sm:p-8">
         <div class="h-full flex flex-col justify-center space-y-4">
           <div class="flex flex-wrap items-center justify-center">
             {#each session.speakers as speaker}
@@ -16,16 +16,16 @@
                   alt={`${speaker.familyNameJp} ${speaker.firstNameJp} Logo`}
                   loading="lazy"
                 />
-                <h6 class="text-lg text-white font-semibold leading-none">
+                <h6 class="text-lg font-semibold leading-none">
                   {`${speaker.familyNameJp} ${speaker.firstNameJp}`}
                 </h6>
-                <span class="text-xs text-white">
+                <span class="text-xs text-gray-500">
                   {`${speaker.familyNameEn} ${speaker.firstNameEn}`}
                 </span>
               </a>
             {/each}
           </div>
-          <p class="text-white md:text-xl px-24 md:px-2">
+          <p class="text-gray-600 md:text-xl px-24 md:px-2">
             {@html session.description}
           </p>
         </div>
--- src/lib/info/SpeakerInfo.svelte
+++ src/lib/info/SpeakerInfo.svelte
@@ -4,18 +4,18 @@
   export let speaker
 </script>
 
-<div class="py-16 bg-revup-medium-brand">
+<div class="py-16">
   <div class="container mt-16 mb-16 mx-auto p-4 md:p-0">
     <div class="shadow-lg flex flex-wrap w-full lg:w-4/5 mx-auto">
       <div
-        class="bg-cover bg-bottom w-full md:w-1/3 h-64 md:h-auto relative"
+        class="bg-cover bg-bottom border w-full md:w-1/3 h-64 md:h-auto relative"
         use:style={{ 'background-image': `url(${speaker.image.url})` }}
       >
         <div class="absolute text-xl">
           <i class="fa fa-heart text-white hover:text-red-light ml-4 mt-4 cursor-pointer" />
         </div>
       </div>
-      <div class="bg-white w-full md:w-2/3 bg-revup-medium-brand">
+      <div class="bg-white w-full md:w-2/3">
         <div class="h-full mx-auto px-6 md:px-0 md:pt-6 md:-ml-6 relative">
           <div
             class="bg-white lg:h-full p-6 -mt-6 md:mt-0 relative mb-4 md:mb-0 flex flex-wrap md:flex-wrap items-center"
