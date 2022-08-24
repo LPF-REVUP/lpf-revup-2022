3c06a50637896a445e902d7e535e2d4613c91235
jiyuujin <jiyuujinunite@gmail.com>
2022-08-24 15:54:56 +0900

    feat: Analytics 4

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,4 +4,5 @@ interface ImportMetaEnv {
   VITE_LIFF_ID: string
   VITE_MICROCMS_API_KEY: string
   VITE_MICROCMS_API_ENDPOINT: string
+  VITE_GTAG_ID: string
 }
--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -2,4 +2,5 @@ export const variables = {
   liffId: import.meta.env.VITE_LIFF_ID,
   microCmsApiKey: import.meta.env.VITE_MICROCMS_API_KEY,
   microCmsApiEndpoint: import.meta.env.VITE_MICROCMS_API_ENDPOINT,
+  gtagId: import.meta.env.VITE_GTAG_ID,
 }
