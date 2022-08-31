3f1a2c9afff7c5ad183dbfec9723393458eeb22d
jiyuujin <jiyuujinunite@gmail.com>
2022-08-31 15:14:59 +0900

    plugins update, others
    - update routes (kit)
    - team, sponsor and speaker style

--- src/routes/session/[id]/+page.svelte
+++ src/routes/session/[id]/+page.svelte
@@ -1,31 +1,25 @@
-<script context="module" lang="ts">
-  import { fetchResBy } from '$lib/services/content.service'
-  import { ogTitle } from '$lib/utils/constants'
-  export async function load({ params }) {
-    let sessionRes = await fetchResBy('sessions', params.id)
-    return { props: { session: sessionRes } }
-  }
-</script>
-
 <script lang="ts">
   import SessionInfo from '$lib/info/SessionInfo.svelte'
   import SocialSection from '$lib/pages/SocialSection.svelte'
-  export let session
+
+  import { ogTitle } from '$lib/utils/constants'
+
+  export let data
 </script>
 
 <svelte:head>
   <html lang="ja" />
-  <title>{`${session.title} | ${ogTitle}`}</title>
-  <meta name="description" content={session.description} />
-  <meta name="twitter:title" content={`${session.title} | ${ogTitle}`} />
-  <meta name="twitter:description" content={session.description} />
+  <title>{`${data.session.title} | ${ogTitle}`}</title>
+  <meta name="description" content={data.session.description} />
+  <meta name="twitter:title" content={`${data.session.title} | ${ogTitle}`} />
+  <meta name="twitter:description" content={data.session.description} />
 </svelte:head>
 
 <div class="bg-revup-top">
   <div class="flex items-center justify-center h-48">
-    <h1 class="text-white text-4xl">{session.title}</h1>
+    <h1 class="text-white text-4xl">{data.session.title}</h1>
   </div>
 </div>
 
-<SessionInfo {session} />
+<SessionInfo session={data.session} />
 <SocialSection />
--- src/routes/speaker/[id]/+page.svelte
+++ src/routes/speaker/[id]/+page.svelte
@@ -1,44 +1,39 @@
-<script context="module" lang="ts">
-  import { fetchResBy } from '$lib/services/content.service'
-  import { ogTitle, websiteUrl } from '$lib/utils/constants'
-  export async function load({ params }) {
-    let speakerRes = await fetchResBy('speakers', params.id)
-    return { props: { id: params.id, speaker: speakerRes } }
-  }
-</script>
-
 <script lang="ts">
   import SpeakerInfo from '$lib/info/SpeakerInfo.svelte'
   import SocialSection from '$lib/pages/SocialSection.svelte'
-  export let id
-  export let speaker
+
+  import { ogTitle, websiteUrl } from '$lib/utils/constants'
+
+  export let data
 </script>
 
 <svelte:head>
   <html lang="ja" />
-  <title>{`${speaker.familyNameJp} ${speaker.firstNameJp} | ${ogTitle}`}</title>
+  <title>{`${data.speaker.familyNameJp} ${data.speaker.firstNameJp} | ${ogTitle}`}</title>
   <meta
     name="description"
-    content={`${speaker.familyNameJp}${speaker.firstNameJp}の自己紹介ページです。`}
+    content={`${data.speaker.familyNameJp}${data.speaker.firstNameJp}の自己紹介ページです。`}
   />
   <meta
     name="twitter:title"
-    content={`${speaker.familyNameJp} ${speaker.firstNameJp} | ${ogTitle}`}
+    content={`${data.speaker.familyNameJp} ${data.speaker.firstNameJp} | ${ogTitle}`}
   />
   <meta
     name="twitter:description"
-    content={`${speaker.familyNameJp}${speaker.firstNameJp}の自己紹介ページです。`}
+    content={`${data.speaker.familyNameJp}${data.speaker.firstNameJp}の自己紹介ページです。`}
   />
-  <meta name="twitter:url" content={`${websiteUrl}/speaker/${id}`} />
-  <meta name="twitter:creator" content={`@${speaker.twitter}`} />
-  <meta name="twitter:site" content={`A;@${speaker.twitter}`} />
+  <meta name="twitter:url" content={`${websiteUrl}/speaker/${data.id}`} />
+  <meta name="twitter:creator" content={`@${data.speaker.twitter}`} />
+  <meta name="twitter:site" content={`A;@${data.speaker.twitter}`} />
 </svelte:head>
 
 <div class="bg-revup-top">
   <div class="flex items-center justify-center h-48">
-    <h1 class="text-white text-4xl">{`${speaker.familyNameJp} ${speaker.firstNameJp}`}</h1>
+    <h1 class="text-white text-4xl">
+      {`${data.speaker.familyNameJp} ${data.speaker.firstNameJp}`}
+    </h1>
   </div>
 </div>
 
-<SpeakerInfo {speaker} />
+<SpeakerInfo speaker={data.speaker} />
 <SocialSection />
