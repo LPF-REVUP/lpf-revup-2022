4a264ad8177ed0b81fbf8266752eb2b9934f2756
jiyuujin <jiyuujinunite@gmail.com>
2022-08-31 18:42:34 +0900

    update features

--- src/lib/feature.ts
+++ src/lib/feature.ts
@@ -1,6 +1,6 @@
 export const TEAM = true
-export const SPEAKER = false
-export const SPONSOR = false
-export const TIMETABLE = false
-export const START_PROPOSAL = true
+export const SPEAKER = true
+export const SPONSOR = true
+export const TIMETABLE = true
+export const START_PROPOSAL = false
 export const SHOW_YOUTUBE = false
