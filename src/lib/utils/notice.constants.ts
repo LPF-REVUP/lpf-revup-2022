a7a31e4fc48c89fa9418f7f03af5abcc6309209b
Yuma Kitamura <jiyuujinunite@gmail.com>
2022-09-20 14:53:15 +0900

    Revert "[release] update features"

--- src/lib/utils/notice.constants.ts
+++ src/lib/utils/notice.constants.ts
@@ -5,10 +5,6 @@ interface NoticeConstants {
 
 export const notices: NoticeConstants[] = [
   {
-    date: '2022-09-20',
-    title: '一般参加者の申込が開始しました！',
-  },
-  {
     date: '2022-08-24',
     title: '公式ティザーサイトがオープンしました！',
   },
