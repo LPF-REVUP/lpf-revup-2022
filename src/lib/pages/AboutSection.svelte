51265c07c38c2cb1a9c50b9f79ed50f307081a4c
jiyuujin <jiyuujinunite@gmail.com>
2022-09-21 20:20:20 +0900

    SHOW_ARCHIVE to false

--- src/lib/pages/AboutSection.svelte
+++ src/lib/pages/AboutSection.svelte
@@ -44,7 +44,7 @@
         <div class="ml-2 md:w-3/5 w-full text-lg text-white">無料</div>
       </div>
     </div>
-    {#if !SHOW_ARCHIVE}
+    {#if SHOW_ARCHIVE}
       <div class="py-4 flex justify-center">
         <LinkPreview {archiveInfo} />
       </div>
