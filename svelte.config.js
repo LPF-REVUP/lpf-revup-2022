517db45abf54f43086dfb252097fd5b1ec3de9da
jiyuujin <jiyuujinunite@gmail.com>
2023-03-08 21:56:58 +0900

    plugins update
    - update kit

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,18 @@
-import preprocess from 'svelte-preprocess'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 import static_adapter from '@sveltejs/adapter-netlify'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: vitePreprocess({
+    postcss: true,
+  }),
 
   kit: {
     adapter: static_adapter(),
     prerender: {
       crawl: process.env.NODE_ENV !== 'development',
-      enabled: process.env.NODE_ENV !== 'development',
     },
 
     paths: { assets: '', base: '' },
